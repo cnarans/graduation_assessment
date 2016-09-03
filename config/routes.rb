5e96c94eafa69458f62366985fc362576556a708
Colin Narans <cnarans@gmail.com>
2016-09-03 13:41:59 -0500

    messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
   get 'home/index'
-  resources :entries
+  resources :entries, :messages
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
